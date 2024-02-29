ce1d7c4f1820d7dfe1267e2fb4f9d225e7dd2df5
周家頡 <jackchou@zhoujiajiadeMacBook-Pro.local>
2024-02-29 13:03:07 +0800

    add Environment variables

--- src/supabase.js
+++ src/supabase.js
@@ -1,8 +1,8 @@
 import { createClient } from "@supabase/supabase-js";
+import("dotenv/config");
 
 const supabaseUrl = "https://dbxnteypzchcfnwqyirn.supabase.co";
-const supabaseKey = "";
-
+const supabaseKey = process.env.API_KEY;
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 export default supabase;
